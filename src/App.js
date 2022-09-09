fd39736d531ad5540b11769c32b82f72f3a793e3
Ashhar909 <ashhar374@gmail.com>
2022-09-09 18:59:14 +0530

    routes2

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css';
 import Privacy from './components/miscellaneous/Privacy';
 import Careers from './components/home/Careers';
-import Terms from './components/miscellaneous/Terms';
 import Home from './components/home/Home';
 import Navbar from './components/constants/Navbar';
+import Faqs from './components/home/Faqs'
+import Voyage from './components/explore/Bon-Voyage';
+import Powershot from './components/explore/Powershot';
+import Skill from './components/miscellaneous/Skill';
+import Refund from './components/miscellaneous/Refund'
+import Pricing from './components/miscellaneous/Pricing'
+import Terms from './components/miscellaneous/Terms';
+
 
 function App() {
   return (
@@ -15,8 +22,16 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/careers" element={<Careers />} />
+          <Route exact path="/faqs" element={<Faqs />} />
+
+          <Route exact path="/voyage" element={<Voyage />} />
+          <Route exact path="/powershot" element={<Powershot />} />
+
           <Route exact path="/privacy" element={<Privacy />} />
           <Route exact path="/terms" element={<Terms />} />
+          <Route exact path="/skill" element={<Skill />} />
+          <Route exact path="/refund" element={<Refund />} />
+          <Route exact path="/pricing" element={<Pricing />} />
         </Routes>
       </div>
     </BrowserRouter>
