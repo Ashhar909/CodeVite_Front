a9261a1be46b454418c8f3834471bcef1a1aa17f
devyanit05 <thokaldevyani123@gmail.com>
2022-09-09 16:41:46 +0530

    hbbd

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import './App.css';
 
 function App() {
   return (
-<<<<<<< HEAD
     <h1>Home</h1>
-=======
-    <div className="App">
-      <h1>Hey</h1>
-    </div>
->>>>>>> origin/master
   );
 }
 
