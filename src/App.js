098188e091a8d9dab03d7180e82cb6efba2221ee
Ashhar909 <ashhar374@gmail.com>
2022-09-09 18:00:28 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css';
 import Privacy from './components/miscellaneous/Privacy';
+import Careers from './components/home/Careers';
+import Terms from './components/miscellaneous/Terms';
+import Home from './components/home/Home';
 
 function App() {
   return (
-    <div className="App">
-      {/* <h1>Hey</h1> */}
-      <Privacy />
+    <BrowserRouter>
+    <Navbar/>
+    {/* <Alert message="hello alert"/> */}
+    <div className='container'>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/careers" element={<Careers />} />
+      <Route exact path="/privacy" element={<Privacy />} />
+      <Route exact path="/terms" element={<Terms/>} />
+    </Routes>
     </div>
+  </BrowserRouter>
   );
 }
 
