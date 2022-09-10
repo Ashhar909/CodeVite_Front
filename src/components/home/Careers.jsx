c079749fc904a7d3a008b72b7b8fd9443003bb37
Ashhar909 <ashhar374@gmail.com>
2022-09-10 22:17:52 +0530

    career

--- src/components/home/Careers.jsx
+++ src/components/home/Careers.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import React from "react";
+import Career from "../../assets/Career.jpeg";
 
 const Careers = () => {
   return (
-    <div>Careers</div>
-  )
-}
+    <div>
+      <h1>
+        Careers @ <b>Code</b>Vita Live
+      </h1>
+      <div className="container d-flex justify-content-around my-3">
+        <div className="container">
+          <img
+            src={Career}
+            alt="..."
+            style={{ width: "600px", height: "380px" }}
+          />
+          <div style={{ marginTop: "20px", textAlign: "justify" }}>
+            If you are ambitious, astute, dynamic and willing to take your
+            career to the next level by joining hands with an organization that
+            believes in creating an impact and has philanthropy at the heart of
+            everything it does, then you are at the right place ! <br />
+            <br />
+            Join us in our mission of disrupting the ed-tech industry and
+            converting children into future STEM leaders and embark in an
+            enthralling career trajectory with us!
+            <br />
+            <br />
+            Fill this short interest form up and our officials will be in touch
+            with you shortly with further instructions.
+            <br />
+            <br /> Please note that we are done hiring for non-technical
+            profiles for the 2020-21 season.
+          </div>
+        </div>
 
-export default Careers
+        <div className="container">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="exampleInputPassword1"
+                required
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                required
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Phone Number
+              </label>
+              <input
+                type="number"
+                className="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                College
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                required
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+              Post You Are Applying For( Content Developer/SDE/BDA/Web Developer)
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                required
+              />
+            </div>
+            
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+              <div id="emailHelp" className="form-text">
+              We are based on Pune, India and all non-technical roles are WFH until further notice.
+              </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Careers;
